80a4ff9cfa7872016b86f8145d59272580bb3ba4
sumit3105 <gohilsumit15@gmail.com>
2025-01-04 10:57:53 +0530

    updated execute function

--- server/execute.js
+++ server/execute.js
@@ -2,7 +2,8 @@ const baseUrl = 'https://judge0-ce.p.rapidapi.com';
 const apiKey = process.env.API_KEY;
 
 async function execute(code) {
-  const url = `${baseUrl}/submissions?base64_encoded=false&wait=true`;
+  const url = `${baseUrl}/submissions?base64_encoded=true&wait=true`;
+  let encoded_code = btoa(code);
   const options = {
     method: 'POST',
     headers: {
@@ -12,13 +13,14 @@ async function execute(code) {
     },
     body: JSON.stringify({
       language_id: 52,
-      source_code: code,
+      source_code: encoded_code,
     }),
   };
 
   try {
     const response = await fetch(url, options);
     const result = await response.json();
+    console.log(result);
     return result;
   } catch (error) {
     console.error('Error in execute:', error);
@@ -26,49 +28,23 @@ async function execute(code) {
   }
 }
 
-async function getSubmission(token) {
-  const url = `${baseUrl}/submissions/${token}?base64_encoded=false&fields=stdout,stderr,status_id,compile_output`;
-  const options = {
-    method: 'GET',
-    headers: {
-      'x-rapidapi-key': apiKey,
-      'x-rapidapi-host': 'judge0-ce.p.rapidapi.com',
-    },
-  };
-
-  try {
-
-    const response = await fetch(url, options);
-    const res = await response.json();
-    console.log(res);
-    if (res.status_id === 3) {
-      return { type: 'stdout', output: res.stdout };
+async function main(code) {
+  const submissionResult = await execute(code);
+  if (submissionResult && submissionResult.token) {
+    if (submissionResult.status.id === 3) {
+      return { type: 'stdout', output: atob(submissionResult.stdout) };
     }
-    else if (res.status_id >= 4) {
-      if (res.compile_output) {
-        return { type: 'stderr', output: res.compile_output };
+    else {
+      if (submissionResult.compile_output) {
+        return { type: 'stderr', output: atob(submissionResult.compile_output) };
       }
-      else if (res.stderr) {
-        return { type: 'stderr', output: res.stderr };
+      else if (submissionResult.stderr) {
+        return { type: 'stderr', output: atob(submissionResult.stderr) };
       }
       else {
         return { type: 'stderr', output: 'Unknown Error.' };
       }
     }
-    else {
-      console.log('Still processing... Retrying in 1 second.');
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-    }
-  } catch (error) {
-    console.error('Error in getSubmission:', error);
-    throw new Error('Failed to retrieve submission result.');
-  }
-}
-
-async function main(code) {
-  const submissionResult = await execute(code);
-  if (submissionResult && submissionResult.token) {
-    return await getSubmission(submissionResult.token);
   } else {
     throw new Error('Submission token not found!');
   }
--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-  console.log("User is connected: " + socket.id);
+  console.log("User connected: " + socket.id);
 });
 
 // Handle "/api/code/execute" route
